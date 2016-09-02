781e43a8c3f84cc75db8710b512e4bca621814ce
Cybergartar <kamonnop.a@gmail.com>
2016-09-03 01:15:30 +0700

    edit modifier in Student class

--- Student.java
+++ Student.java
@@ -2,43 +2,40 @@ package Ass0010Sec1;
 
 import java.util.ArrayList;
 
-/**
- * Created by ultimate on 9/2/16.
- */
 class Student {
     private String id, name, surname;
     private int credits;
     private ArrayList<String> enrolledSubject = new ArrayList<>();
     private boolean submitStatus;
 
-    public Student(String... info) {
+    Student(String... info) {
         this.id = info[0];
         this.name = info[1];
         this.surname = info[2];
         this.submitStatus = false;
     }
 
-    public String getId() {
+    String getId() {
         return id;
     }
 
-    public String getName() {
+    String getName() {
         return name;
     }
 
-    public String getSurname() {
+    String getSurname() {
         return surname;
     }
 
-    public int getCredits(){
+    int getCredits(){
         return credits;
     }
 
-    public void setCredits(int credits) {
+    void setCredits(int credits) {
         this.credits = credits;
     }
 
-    public boolean enroll(String subject, int subjectCredit){
+    boolean enroll(String subject, int subjectCredit){
         int currentCredit = getCredits();
         if(currentCredit + subjectCredit > 22){
             UI.printError("Cannot enroll! (Maximum credits exceed)");
